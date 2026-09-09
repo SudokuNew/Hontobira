280b5708c5b83d63d3824da07a8f65b8b3c7ed47
SudokuNew <sudoku.feb@gmail.com>
2026-09-09 20:02:26 +0900

    Add Color

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Home from "./pages/Home";
@@ -9,15 +10,29 @@ import "./components/GeneratedCover.css";
 import "./components/StarRating.css";
 import "./components/ShelfControls.css";
 
+// ページ遷移のたびに一番上までスクロールを戻す。
+// これがないと、詳細ページの途中で「書架にもどる」を押しても
+// スクロール位置がそのまま引き継がれ、戻ったように見えない/反応していないように感じてしまう。
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+  return null;
+}
+
 export default function App() {
   const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/book/:id" element={<BookDetail />} />
-      </Routes>
-    </AnimatePresence>
+    <>
+      <ScrollToTop />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/book/:id" element={<BookDetail />} />
+        </Routes>
+      </AnimatePresence>
+    </>
   );
 }
